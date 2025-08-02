a68c48750e54e771db399b66c643e412880ea102
Vianpyro <vianney@veremme.org>
2025-08-02 20:08:03 +0000

    Add build argument for API URL in Dockerfile and set base path in Astro config

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+    base: '/penny_game/',
     build: {
         format: 'file',
         assets: 'compressed',
