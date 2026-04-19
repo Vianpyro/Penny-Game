86aa2141e98337903768b30101e8720a15f02eff
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2026-04-18 21:29:47 -0400

    Change base path in astro.config.mjs

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/penny_game/',
+    base: '/',
     build: {
         format: 'file',
         assets: 'compressed',
